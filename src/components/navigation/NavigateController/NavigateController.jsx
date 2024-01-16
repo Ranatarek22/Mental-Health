663421a03dd50d9c77045b7340c304196d67cb79
Ranatarek22 <rt589611@gmail.com>
2024-01-16 18:41:54 +0200

    Fix the route navigation

---
+++ src/components/navigation/NavigateController/NavigateController.jsx
@@ -0,0 +1,18 @@
+import React, { useEffect } from "react";
+import Navbar from "../NavBar/navbar";
+import Sidebar from "../SideBar/sidebar";
+import { useLocation } from "react-router-dom";
+import { main_routes } from "../../../routes/routes";
+// const main_routes = ["/", "/login", "/signup"];
+const NavigateController = (props) => {
+  const loc = useLocation();
+  //   console.log(loc);
+  const { setPathname } = props;
+  const pathname = loc.pathname;
+  useEffect(() => {
+    setPathname(loc.pathname);
+  }, [loc.pathname]);
+  return <>{main_routes.includes(pathname) ? <Navbar /> : <Sidebar />}</>;
+};
+
+export default NavigateController;
