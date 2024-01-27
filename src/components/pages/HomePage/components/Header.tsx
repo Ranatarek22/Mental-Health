2f40f5af66272616028dd853dc863f8ffbfc9574
shebll <shebblloll@gmail.com>
2024-01-27 14:23:35 +0200

    landing page components

---
+++ src/components/pages/HomePage/components/Header.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import Logo from "./Logo";
+import NavBar from "./NavBar";
+
+export default function Header() {
+  return (
+    <header className="text-gray-700">
+      <section className="container mx-auto p-4 flex justify-between items-center overflow-hidden">
+        <Logo />
+        <ul className="hidden md:flex gap-4 items-center justify-center ">
+          <a href={"#About"}>
+            <li className="hover:text-gray-700 transition-all md:text-xl">
+              About
+            </li>
+          </a>
+          <a href={"#Services"}>
+            <li className="hover:text-gray-700 transition-all md:text-xl">
+              Services
+            </li>
+          </a>
+          <a href={"#Doctors"}>
+            <li className="hover:text-gray-700 transition-all md:text-xl">
+              Doctors
+            </li>
+          </a>
+          <a href={"#Articles"}>
+            <li className="hover:text-gray-700 transition-all md:text-xl">
+              Articles
+            </li>
+          </a>
+        </ul>
+        <div className="hidden md:flex gap-4">
+          <a href={"/login"}>
+            <button className="border-2 border-[#e4ffe5] text-black font-medium px-4 py-2 rounded-full shadow-lg transition-all hover:scale-105 hover:bg-[#b8ddb9]">
+              Login
+            </button>
+          </a>
+          <a href={"/signup"}>
+            <button className="bg-[#1d491f] text-white font-medium px-4 py-2 rounded-full shadow-lg transition-all hover:scale-105 hover:bg-[#143116]">
+              Get Start
+            </button>
+          </a>
+        </div>
+        <NavBar />
+      </section>
+    </header>
+  );
+}
