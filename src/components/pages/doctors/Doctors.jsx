cd601c0776bc448b600eb9bd9e41764bcb80bc40
shebll <shebblloll@gmail.com>
2024-06-22 22:27:55 +0300

    update filter doctors list using RLSearchParams

--- src/components/pages/doctors/Doctors.jsx
+++ src/components/pages/doctors/Doctors.jsx
@@ -1,87 +1,79 @@
-// src/components/DoctorsList.js
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import { DoctorCard } from "./DoctorCard";
 import { apiInstance } from "../../../axios";
 import { useInView } from "react-intersection-observer";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import useDoctorStore from "../../../hooks/use-doctor-store";
 import { Slider } from "@mui/material";
+import debounce from "lodash.debounce";
 
-import NavUser from "../../navigation/NavUser/NavUser";
 const DoctorsList = () => {
   const [doctors, setDoctors] = useState([]);
-  const [filteredDoctors, setFilteredDoctors] = useState([]);
-  const [hasMore, setHasMore] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
+  const [hasMore, setHasMore] = useState(false);
   const [page, setPage] = useState(1);
-  const [filters, setFilters] = useState({
-    name: "",
-    specialization: "",
-    gender: "",
-    city: "",
-    minFees: 0,
-    maxFees: 1000,
-  });
   const pageSize = 10;
-  const addDoctor = useDoctorStore((state) => state.setDocs);
 
   const navigate = useNavigate();
+  const location = useLocation();
 
   const { ref, inView } = useInView({
     threshold: 1.0,
   });
 
-  useEffect(() => {
-    const fetchDoctors = async () => {
+  const getFiltersFromURL = () => {
+    const searchParams = new URLSearchParams(location.search);
+    return {
+      name: searchParams.get("name") || "",
+      specialization: searchParams.get("specialization") || "",
+      gender: searchParams.get("gender") || "",
+      city: searchParams.get("city") || "",
+      minFees: parseInt(searchParams.get("minFees")) || 0,
+      maxFees: parseInt(searchParams.get("maxFees")) || 1000,
+    };
+  };
+
+  const [filters, setFilters] = useState(getFiltersFromURL);
+
+  const fetchDoctors = useCallback(
+    debounce(async (page, filters) => {
       setIsLoading(true);
       try {
         const response = await apiInstance.get(
-          `/doctors?PageNumber=${page}&PageSize=${pageSize}`
+          `/doctors?PageNumber=${page}&PageSize=${pageSize}&Name=${filters.name}&Specialization=${filters.specialization}&Gender=${filters.gender}&City=${filters.city}&MinFees=${filters.minFees}&MaxFees=${filters.maxFees}`
         );
         const newData = response.data;
-        addDoctor(newData);
-        setDoctors((prev) => [...prev, ...newData]);
-        setFilteredDoctors((prev) => [...prev, ...newData]);
+        if (page === 1) {
+          setDoctors(newData);
+        } else {
+          setDoctors((prev) => [...prev, ...newData]);
+        }
         setHasMore(newData.length === pageSize);
-        console.log(doctors);
       } catch (error) {
         console.error("Error fetching doctors:", error);
       }
       setIsLoading(false);
-    };
-    fetchDoctors();
-  }, [page]);
+    }, 1000),
+    []
+  );
 
   useEffect(() => {
+    fetchDoctors(page, filters);
+  }, [page, filters]);
+
+  useEffect(() => {
+    console.log(hasMore);
     if (inView && hasMore && !isLoading) {
       setPage((prevPage) => prevPage + 1);
     }
   }, [hasMore, isLoading, inView]);
 
   useEffect(() => {
-    const filtered = doctors.filter((doctor) => {
-      return (
-        (doctor.firstName?.toLowerCase() || "").includes(
-          filters.name.toLowerCase()
-        ) &&
-        (doctor.specialization?.toLowerCase() || "").includes(
-          filters.specialization.toLowerCase()
-        ) &&
-        (doctor.gender?.toLowerCase() || "").includes(
-          filters.gender.toLowerCase()
-        ) &&
-        ((doctor.city?.toLowerCase() || "").includes(
-          filters.city.toLowerCase()
-        ) ||
-          (doctor.location?.toLowerCase() || "").includes(
-            filters.city.toLowerCase()
-          )) &&
-        doctor.sessionFees >= filters.minFees &&
-        doctor.sessionFees <= filters.maxFees
-      );
+    const searchParams = new URLSearchParams(filters);
+    navigate(`${location.pathname}?${searchParams.toString()}`, {
+      replace: true,
     });
-    setFilteredDoctors(filtered);
-  }, [filters, doctors]);
+  }, [filters, navigate, location.pathname]);
 
   const handleFilterChange = (e) => {
     const { name, value } = e.target;
@@ -97,86 +89,80 @@ const DoctorsList = () => {
   };
 
   return (
-    <>
-      <NavUser />
-
-      <div className="doctors-list-container">
-        <div className="filters">
-          <h4>Filter</h4>
-          <input
-            type="text"
-            name="name"
-            placeholder="Name"
-            value={filters.name}
-            onChange={handleFilterChange}
+    <div className="doctors-list-container">
+      <div className="filters">
+        <input
+          type="text"
+          name="name"
+          placeholder="Name"
+          value={filters.name}
+          onChange={handleFilterChange}
+        />
+        <input
+          type="text"
+          name="specialization"
+          placeholder="Specialization"
+          value={filters.specialization}
+          onChange={handleFilterChange}
+        />
+        <input
+          type="text"
+          name="gender"
+          placeholder="Gender"
+          value={filters.gender}
+          onChange={handleFilterChange}
+        />
+        <input
+          type="text"
+          name="city"
+          placeholder="City"
+          value={filters.city}
+          onChange={handleFilterChange}
+        />
+        <div className="fees-filter">
+          <Slider
+            value={[filters.minFees, filters.maxFees]}
+            onChange={handleFeesChange}
+            valueLabelDisplay="auto"
+            min={0}
+            max={1000}
           />
-          <input
-            type="text"
-            name="specialization"
-            placeholder="Specialization"
-            value={filters.specialization}
-            onChange={handleFilterChange}
-          />
-          <input
-            type="text"
-            name="gender"
-            placeholder="Gender"
-            value={filters.gender}
-            onChange={handleFilterChange}
-          />
-          <input
-            type="text"
-            name="city"
-            placeholder="City"
-            value={filters.city}
-            onChange={handleFilterChange}
-          />
-          <div className="fees-filter">
-            <Slider
-              className="Slider"
-              value={[filters.minFees, filters.maxFees]}
-              onChange={handleFeesChange}
-              valueLabelDisplay="auto"
-              min={0}
-              max={1000}
+          <div className="fees-inputs">
+            <input
+              type="number"
+              name="minFees"
+              placeholder="Min Fees"
+              value={filters.minFees}
+              onChange={handleFilterChange}
             />
-            <div className="fees-inputs">
-              <input
-                type="number"
-                name="minFees"
-                placeholder="Min Fees"
-                value={filters.minFees}
-                onChange={handleFilterChange}
-              />
-              <input
-                type="number"
-                name="maxFees"
-                placeholder="Max Fees"
-                value={filters.maxFees}
-                onChange={handleFilterChange}
-              />
-            </div>
+            <input
+              type="number"
+              name="maxFees"
+              placeholder="Max Fees"
+              value={filters.maxFees}
+              onChange={handleFilterChange}
+            />
           </div>
         </div>
+      </div>
 
-        <div className="">
-          {filteredDoctors.map((doctor) => (
-            <div key={doctor.id}>
-              <DoctorCard doctor={doctor} />
-            </div>
+      <div className="">
+        {doctors.map((doctor) => (
+          <div key={doctor.id}>
+            <DoctorCard doctor={doctor} />
+          </div>
+        ))}
+      </div>
+
+      {isLoading && (
+        <div className="DoctorCardSkeleton-list">
+          {[1, 2, 3, 4, 5, 6, 7, 8].map((_, idx) => (
+            <DoctorCardSkeleton key={idx} />
           ))}
         </div>
-
-        {isLoading && (
-          <div className="DoctorCardSkeleton-list">
-            {[1, 2, 3, 4, 5, 6, 7, 8].map((_, idx) => (
-              <DoctorCardSkeleton key={idx} />
-            ))}
-          </div>
-        )}
-        <div ref={ref} />
-      </div>
-    </>
+      )}
+      <div ref={ref} />
+    </div>
   );
 };
 
